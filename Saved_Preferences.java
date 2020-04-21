d596abf8f582dd6e6c9974721a1ea684f7528c62
shivam-raj-4 <53818754+shivam-raj-4@users.noreply.github.com>
2020-04-21 16:53:39 +0530

    Create Saved_Preferences.java

---
+++ Saved_Preferences.java
@@ -0,0 +1,23 @@
+Mainactivity
+
+public class MainActivity extends AppCompatActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+}
+public void loadActivityB(View v){
+Intent i=new Intent(MainActivity.this, ActivityB.class);
+startActivity(i);
+}
+}
+
+ActivityB
+
+public class ActivityB extends PreferenceActivity {
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+//setContentView(R.layout.activity_layout_b);
+addPreferencesFromResource(R.xml.mypreferences);}
+}
